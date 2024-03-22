09da2417de82b6e10c22abc19cb609c2056192f4
elleveen <eleonoramanushkova@yandex.ru>
2024-03-22 20:16:09 +0700

    registration

---
+++ views/user/_form.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var app\models\User $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="user-form">
+
+    <?php $form = ActiveForm::begin([
+        'id' => 'registration-form',
+    ]);
+    ?>
+
+    <?= $form->field($model, 'fullname')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'password_repeat')->passwordInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'check')->checkbox() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Зарегистрироваться', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
